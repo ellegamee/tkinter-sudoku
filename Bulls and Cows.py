88c6916f7017ff978aec81e6cf79329812cb3c19
Elias Axonov <ed.axonov@gmail.com>
2021-03-03 15:32:01 +0100

    Bull and Cows
    Update

--- Bulls and Cows.py
+++ Bulls and Cows.py
@@ -2,7 +2,7 @@
 import random
 code = []
 player = []
-output = ['_','_','_','_']
+output = ['_', '_', '_', '_']
 guess = 0
 guesses = 0
 maxvalue = 9
@@ -10,50 +10,67 @@ minvalue = 0
 bulls = 0
 cows = 0
 goats = 0
-for i in range (0,4) :
-  code.append(random.randint(0,9))
 
-while guesses < 11 :
-  player = []
-  for i in range(0,4) :
-    while True :
-      try :
-        guess = int(input('Skriv in ett tal mellan {} och {}: '.format(minvalue,maxvalue)))
+# Random code
+for i in range(0, 4):
+    code.append(random.randint(0, 9))
+print(code)
 
-      except :
-        print('Skriv in ett tal mellan {} och {}\n '.format(minvalue,maxvalue))
-        continue
+while guesses < 11:
+    # Player guess list
+    player = []
 
+    # Players input
+    for i in range(0, 4):
+        while True:
+            try:
+                guess = int(
+                    input('Skriv in ett tal mellan {} och {}: '.format(minvalue, maxvalue)))
 
-      if guess < minvalue or guess > maxvalue :
-        print('Skriv in ett tal mellan {} och {}\n '.format(minvalue,maxvalue))
-        continue
+            except:
+                print('Skriv in ett tal mellan {} och {}\n '.format(
+                    minvalue, maxvalue))
+                continue
 
-      else :
-        player.append(guess)
-        break
-  print(code)
-  print(player)
-  bulls = 0
-  cows = 0
-  goats = 0
-  for j in range(0,len(player)) :
-    if player[j] == code[j] :
-      output.pop(j)
-      output.insert(j,'B')
-      bulls += 1
-   
-    else :
-      for k in range(0,len(player)) :
-        if player[k] == code[j] :
-          print(player[k],code[j])
-          #output.pop(j)
-          output.insert(j,'C')
-          cows += 1
-        else : 
-          print(player[k],code[j])
-          #output.pop(j)
-          output.insert(j,'G')
-          goats += 1
-  print(output,'\nAntal Bulls {}, Antal Cows {}, Antal Goats {}\n'.format(bulls,cows,goats))
-  guesses += 1
+            if guess < minvalue or guess > maxvalue:
+                print('Skriv in ett tal mellan {} och {}\n '.format(
+                    minvalue, maxvalue))
+                continue
+
+            else:
+                player.append(guess)
+                break
+
+    # Debug
+    print(code)
+    print(player)
+
+    # Compare
+    bulls = 0
+    cows = 0
+    goats = 0
+
+    # If they are same
+    for j in range(0, len(player)):
+
+        # Bull
+        if player[j] == code[j]:
+            output.pop(j)
+            output.insert(j, 'B')
+            bulls += 1
+
+        # Cow
+        elif player[j] in code:
+            output.pop(j)
+            output.insert(j, 'C')
+            cows += 1
+
+        # Goat
+        else:
+            output.pop(j)
+            output.insert(j, 'G')
+            goats += 1
+
+    print(output, '\nAntal Bulls {}, Antal Cows {}, Antal Goats {}\n'.format(
+        bulls, cows, goats))
+    guesses += 1
