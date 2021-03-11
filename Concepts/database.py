bce0a8008440d8c82400c5e66aed1ace869c4e7f
ellegamee <ed.axonov@gmail.com>
2021-03-11 12:04:24 +0100

    Update database.py
    Small patch, need a list inside dubble  dict.

--- Concepts/database.py
+++ Concepts/database.py
@@ -1,6 +1,6 @@
-data = {"Rows": {"r1": "row1", "r2": "row2", "r3": "row3"},
-        "Collums": {"c1": "collum1", "c2": "collum2", "c3": "collum3"},
-        "Square": {"s1": "square1", "s2": "square2", "s3": "square3"},
+data = {"Rows": {"r1": [], "r2": [], "r3": []},
+        "Collums": {"c1": [], "c2": [], "c3": []},
+        "Square": {"s1": [], "s2": [], "s3": []},
         "Entire": ["Entire but not really"]}
 
 print(data["Square"]["s1"])
