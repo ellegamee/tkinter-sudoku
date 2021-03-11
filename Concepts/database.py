02f9252d30994265b0603cdcf8e8da39e4d73b3d
ellegamee <ed.axonov@gmail.com>
2021-03-11 12:41:15 +0100

    Update database.py
    Added varible and change in name of database. Easier to understand.

--- Concepts/database.py
+++ Concepts/database.py
@@ -1,8 +1,9 @@
-data = {"Rows": {"r1": [], "r2": [], "r3": []},
-        "Collums": {"c1": [], "c2": [], "c3": []},
-        "Square": {"s1": [], "s2": [], "s3": []},
-        "Entire": ["Entire but not really"]}
+gameboard = {"Base": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+             "Rows": {"r1": [], "r2": [], "r3": []},
+             "Collums": {"c1": [], "c2": [], "c3": []},
+             "Square": {"s1": [], "s2": [], "s3": []},
+             "Entire": ["Entire but not really"]}
 
 # Easy way to point at a number
-print(data["Square"]["s1"])
-print(data["Entire"])
+print(gameboard["Square"]["s1"])
+print(gameboard["Entire"])
