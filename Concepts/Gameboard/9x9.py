f11ecc4e9704c362543a14148ab580113217f627
ellegamee <ed.axonov@gmail.com>
2021-03-11 12:22:26 +0100

    Gameboard concept
    This is a concept that was developed in another project. Now inserted into this one for faster develoupment.

---
+++ Concepts/Gameboard/9x9.py
@@ -0,0 +1,32 @@
+from tkinter import *
+from tkinter.ttk import *
+import time
+
+root = Tk()
+root.title("9x9")
+root.geometry("400x350")
+
+# Style on lable
+style = Style()
+style.configure("TButton", font=("calibri", 15, "bold"), height=10, width=3)
+
+# List of labels
+rows = [[], [], [], [], [], [], [], [], []]
+for index in range(9):
+    for number in range(1, 10):
+        rows[index].append(Button(root, text=number, style="TButton"))
+
+# Visualize all of them
+for count, button in enumerate(rows):
+    for index in range(9):
+        button[index].grid(row=index, column=count)
+
+"""
+# Change the first one of all
+button = rows[0][0]
+button.configure(text="2")
+button.update()
+"""
+
+# Print out the program
+root.mainloop()
---
+++ Concepts/Gameboard/9x9_modified.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from tkinter.ttk import *
+import random
+
+root = Tk()
+root.title("9x9")
+root.geometry("400x350")
+
+# Style on lable
+style = Style()
+style.configure("TButton", font=("calibri", 15, "bold"), height=10, width=3)
+
+# List of labels
+first_row = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+random.shuffle(first_row)
+
+rows = [[], [], [], [], [], [], [], [], []]
+for index in range(9):
+    if index == 0:
+        for number in first_row:
+            rows[index].append(Button(root, text=number, style="TButton"))  
+    else:
+        for _ in range(9):      
+            rows[index].append(Button(root, text="-", style="TButton"))
+
+# Visualize all of them
+for count, button in enumerate(rows):
+    for index in range(9):
+        button[index].grid(row=count, column=index)
+
+"""
+# Change the first one of all
+button = rows[0][0]
+button.configure(text="2")
+button.update()
+"""
+
+# Print out the program
+root.mainloop()
