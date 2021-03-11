2c72920bda32f4add1fbb76c2517e65d8da0b809
ellegamee <ed.axonov@gmail.com>
2021-03-11 09:49:25 +0100

    Create database.py
    Concept of how the database with numbers will be stored to be effective.

---
+++ Concepts/database.py
@@ -0,0 +1,7 @@
+data = {"Rows": {"r1": "row1", "r2": "row2", "r3": "row3"},
+        "Collums": {"c1": "collum1", "c2": "collum2", "c3": "collum3"},
+        "Square": {"s1": "square1", "s2": "square2", "s3": "square3"},
+        "Entire": ["Entire but not really"]}
+
+print(data["Square"]["s1"])
+print(data["Entire"])
