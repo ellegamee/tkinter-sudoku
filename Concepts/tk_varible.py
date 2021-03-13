0382f3d67ddc08566edcdafcc4c3b2ec28a304c1
ellegamee <ed.axonov@gmail.com>
2021-03-13 01:31:34 +0100

    Update tk_varible.py
    More choices on how we can store data just so we have options for the future.

--- Concepts/tk_varible.py
+++ Concepts/tk_varible.py
@@ -8,10 +8,20 @@ root = Tk()
 # Tkinter variables
 # initialization using constructor
 intvar = IntVar(root, value=25, name="2")
-strvar = StringVar(root, name="string", value="Hello!")
+strvar = StringVar(root, name="string")
 boolvar = BooleanVar(root, True)
 doublevar = DoubleVar(root, 10.25)
 
-print(strvar.get())
+# * Really important!!!!!
+StringVar(root, name="string2")
+
+# * Really important!!!!
+root.setvar(name="string2", value="Test2...")
+print(root.getvar("string2"))
+
+# ! Not really best solution
+print()
 root.setvar(name="string", value="Test...")
+print(strvar.get())
+strvar.set("test2")
 print(strvar.get())
