7ccf4ed7d0911bd2142a0426e9f76ad1707feb45
Elias Axonov <ed.axonov@gmail.com>
2021-03-23 08:33:19 +0100

    Update Varible_9x9.py
    Patched up database so every number get's saved properly.

--- Concepts/Tk_varible/Varible_9x9.py
+++ Concepts/Tk_varible/Varible_9x9.py
@@ -5,17 +5,17 @@ def create_rcs():
     # Creates list where varibles will be stored
     # rcs stands for rows, Collumn, square
     for i in range(1, 10):
-        gb["Rows"]["r{}".format(i)] = []
-        gb["Collumn"]["c{}".format(i)] = []
+        gb["rows"]["r{}".format(i)] = []
+        gb["collumn"]["c{}".format(i)] = []
 
     for i in range(1, 10):
-        gb["Square"]["s{}".format(i)] = []
+        gb["square"]["s{}".format(i)] = []
 
 
 def varible_row():
     row = 1
     for i in range(1, 82):
-        gb["Rows"]["r{}".format(row)].append(
+        gb["rows"]["r{}".format(row)].append(
             StringVar(root, name="{}".format(i)))
 
         if i % 9 == 0:
@@ -28,20 +28,22 @@ def varible_collumn():
         if collumn == 10:
             collumn = 1
 
-        gb["Collumn"]["c{}".format(collumn)].append(
+        gb["collumn"]["c{}".format(collumn)].append(
             StringVar(root, name="{}".format(i)))
-
         collumn += 1
 
 
 def varible_square():
     #! not working in square 3
     square = 1
-    inside = 0
+    inside = 1
     endless = 1
     rerun = 0
 
-    for i in range(1, 81):
+    for i in range(1, 82):
+        gb["square"]["s{}".format(square)].append(
+            StringVar(root, name="{}".format(i)))
+
         if rerun == 2 and endless == 9:
             rerun = 0
             inside = 0
@@ -59,19 +61,15 @@ def varible_square():
             square += 1
             inside = 0
 
-        gb["Square"]["s{}".format(square)].append(
-            StringVar(root, name="{}".format(i)))
-
         inside += 1
         endless += 1
 
-
-# * Main code
-gb = {"Start": [1, 2, 3, 4, 5, 6, 7, 8, 9],
-      "Rows": {},
-      "Collumn": {},
-      "Square": {},
-      "Entire": ["--Undefined--"]}
+    # * Main code
+gb = {"start": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      "rows": {},
+      "collumn": {},
+      "square": {},
+      "entire": ["--Undefined--"]}
 
 # Make tkinter
 root = Tk()
@@ -83,3 +81,6 @@ create_rcs()
 varible_row()
 varible_collumn()
 varible_square()
+
+for i in range(1, 10):
+    print(len(gb["square"]["s{}".format(i)]))
