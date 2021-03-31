77b15b69a24164be8d30ee3a234a7655144546ea
ellegamee <ed.axonov@gmail.com>
2021-03-31 15:11:21 +0200

    Create loop_test.py
    for loop zip test for compacter code and make it more simplified for the proof of concept code.

---
+++ Concepts/loop_test.py
@@ -0,0 +1,11 @@
+import itertools
+
+num = [1, 2, 3]
+color = ["red", "while", "black"]
+value = [255, 256]
+
+# iterates over 3 lists and excutes
+# 2 times as len(value)= 2 which is the
+# minimum among all the three
+for (a, b, c) in zip(num, color, value):
+    print(a, b, c)
