1983ddc5ed48da280d257c3fa3047a6f8d6e81ca
Elias Axonov <ed.axonov@gmail.com>
2021-07-05 20:43:45 +0200

    run function working

--- game.py
+++ game.py
@@ -2,30 +2,20 @@ from tkinter import Tk
 
 
 class Game:
+
     def __init__(self):
+        # ? What to put in here?
+        pass
 
-        class Window:
-            def __init__(self, title, geometry):
-                self.title = title
-                self.geometry = geometry
-
-        class dataBase:
-            def __init__(self, row, column, square, entire, button, start):
-                self.row = row
-                self.column = column
-                self.square = square
-                self.entire = entire
-                self.button = button
-                self.start = start
-
-        dataBase = dataBase({}, {}, {}, [], [], [1, 2, 3, 4, 5, 6, 7, 8, 9])
-        window = Window("Soduko Game", "450x350")
-
+    def run(self):
         root = Tk()
-        root.title(window.title)
-        root.geometry(window.geometry)
-
+        root.title("Soduko Game")
+        root.geometry("450x350")
         root.mainloop
+        input("")
 
 
-Game()
+game = Game()
+
+game.run()
+game()
