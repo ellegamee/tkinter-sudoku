2bdb16c5f4c7d7bdf3d3ee06b99e1e592e11dd76
Elias Axonov <ed.axonov@gmail.com>
2021-07-06 17:00:58 +0200

    big make over of getSquareFor

--- Concepts/Tk_varible/getvaluetest.py
+++ Concepts/Tk_varible/getvaluetest.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from typing import List
 
 root = Tk()
 
@@ -16,3 +17,53 @@ if value in lst:
     print("True")
 else:
     print("False")
+
+
+def getSquareFor(index):
+    row = index // 9
+    column = index % 9
+
+    lst = [[], [], [], [], [], [], [], [], []]
+    start = 0
+    end = 18
+
+    # * Makes all lists
+    for listKey in range(9):
+        for _ in range(3):
+            for value in range(start, end+1, 9):
+                lst[listKey].append(value)
+
+            start += 1
+            end += 1
+
+        if listKey == 2 or listKey == 5:
+            start += 18
+            end += 18
+
+    # * Gets wich square
+    if column // 3 == 0:
+        if row // 3 == 0:
+            return lst[0]
+        if row // 3 == 1:
+            return lst[3]
+        if row // 3 == 2:
+            return lst[6]
+
+    if column // 3 == 1:
+        if row // 3 == 0:
+            return lst[1]
+        if row // 3 == 1:
+            return lst[4]
+        if row // 3 == 2:
+            return lst[7]
+
+    if column // 3 == 2:
+        if row // 3 == 0:
+            return lst[2]
+        if row // 3 == 1:
+            return lst[5]
+        if row // 3 == 2:
+            return lst[8]
+
+
+print(getSquareFor(9))
