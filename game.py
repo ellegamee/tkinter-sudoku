4aef3e3fd0b37c7d3bd31a0f4a13aa11ad115d5a
Elias Axonov <ed.axonov@gmail.com>
2021-07-07 01:27:52 +0200

    Comments over the entire code for describing key parts

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ class DataBase:
         self.button = []
         self.data = []
 
-        # Empty varibles
+        # Generating empty varibles
         for index in range(81):
             self.data.append(IntVar(root, name=str(index)))
 
@@ -20,24 +20,29 @@ class DataBase:
         index = 0
         while loop:
 
+            # Pinch of randomness
+            # Todo make generating with a seed
             random.shuffle(self.validNumbers)
             for count, num in enumerate(self.validNumbers):
 
                 check = True
+                # Is it valid to place number or not
                 if num in self.getRowFor(index, True) or num in self.getColumnFor(index, True) or num in self.getSquareFor(index, True):
                     check = False
 
+                # If okay to set value
                 if check == True:
+                    # Sets value in the dataBase
                     root.setvar(name=str(index), value=num)
                     index += 1
-                    print("num", num)
-                    print("index", index)
                     break
 
+                # If row on gameboard fails to generate
                 if (count + 1) == 9:
-                    print("test")
+                    # Backs index back to start of row
                     index = self.getRowFor(index, False)[0]
 
+                    # Clears all values on row
                     for tempIndex in self.getRowFor(index, False):
                         root.setvar(name=str(tempIndex), value=0)
 
@@ -46,13 +51,14 @@ class DataBase:
             if index == 81:
                 loop = False
 
-        # * Makes copy of entire grid
+        # Makes the answer sheet for the game
         for index in range(81):
             self.boardAnswer.append(root.getvar(str(index)))
 
     def getRowFor(self, index, convertToValue):
         start = index // 9 * 9
 
+        # Return scenarios
         if convertToValue == True:
             lstReturn = []
             for i in range(start, (start + 9)):
@@ -65,6 +71,7 @@ class DataBase:
     def getColumnFor(self, index, convertToValue):
         start = index % 9
 
+        # Return scenarios
         if convertToValue == True:
             lstReturn = []
             for i in range(start, (start + 9*9), 9):
@@ -84,17 +91,23 @@ class DataBase:
 
         # * Makes all lists
         for listKey in range(9):
+
+            # Subsquare index, counts:
+            # 0,9,18...1,10,19 etc.
             for _ in range(3):
                 for value in range(start, end+1, 9):
                     lst[listKey].append(value)
 
+                # Move start to next index
                 start += 1
                 end += 1
 
+            # When first 3 or 6 subsquares are done
             if listKey == 2 or listKey == 5:
                 start += 18
                 end += 18
 
+        # Return scenarios
         if convertToValue == True:
             lstReturn = []
             for i in lst[((column // 3) + (row // 3 * 3))]:
@@ -110,6 +123,7 @@ class Game:
         self.data = DataBase(root)
 
 
+# Game window properties
 root = Tk()
 root.title("Soduko Game")
 root.geometry("450x350")
